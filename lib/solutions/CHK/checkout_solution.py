e9709f966a9475ffacd7acaa400dd5fd89bda3cf
sbx_user1051 <sbx_user1051@169.254.115.113>
2023-06-17 10:16:12 +0000

    Sat Jun 17 10:16:12 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,24 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
+def calculate_price(item_count: dict) -> int:
+    total = 0
+    div, remainder = divmod(item_count["A"], 3)
+    total += div * 130
+    total += remainder * 50
+    div, remainder = divmod(item_count["B"], 2)
+    total += div * 45
+    total += remainder * 30
+    total += item_count["C"] * 20
+    total += item_count["D"] * 15
+    return total
+
+
 def checkout(skus):
-    raise NotImplementedError()
+    item_count = {}
+    for item in skus:
+        if item in item_count.keys():
+            item_count[item] += 1
+        else:
+            item_count[item] = 1
+    return calculate_price(item_count)
+
