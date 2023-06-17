9faac289f908e8b70bf14c01974c6b7a15a00bf2
sbx_user1051 <sbx_user1051@169.254.115.113>
2023-06-17 11:29:37 +0000

    Sat Jun 17 11:29:37 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -58,29 +58,23 @@ def calculate_multi_price(total: int, item: str, count: int):
     return total
 
 
-def calculate_multi_price(total: int, item: str, count: int):
-    if len(item_multi_price_map[item]) == 2:
-        calculate_multi_price_more_than_one_offer(total, item, count)
-    elif len(item_multi_price_map[item]) == 1:
-        div, remainder = divmod(item, 2)
-        total += div * 45
-        total += remainder * 30
-    else:
-        raise ValueError("Too many multiprice offers. Implement.")
+# def calculate_bogof_price(total: int, item: str, count: int):
+#     item_bogof_map[item]
+#     total += item_count["F"] * 10
+#     div, _ = divmod(item_count["F"], 3)
+#     total -= div * 10
 
 
 def calculate_price(item_count: dict) -> int:
     total = 0
     for item, count in item_count.items():
         if item in item_multi_price_map.keys():
-            calculate_multi_price(total, item, count)
+            total = calculate_multi_price(total, item, count)
+        elif item in item_bogof_map.keys():
+            total = calculate_bogof_price(total, item, count)
+        else:
+            total = count * item_price_map[item]
 
-    total = calculate_A(total, item_count)
-    total = calculate_E(total, item_count)
-    total = calculate_B(total, item_count)
-    total = calculate_C(total, item_count)
-    total = calculate_D(total, item_count)
-    total = calculate_F(total, item_count)
     return total
 
 
@@ -96,77 +90,78 @@ def checkout(skus):
     return calculate_price(item_count)
 
 
-#####
-def calculate_A(total: int, item_count: dict):
-    if "A" in item_count.keys():
-        div, remainder = divmod(item_count["A"], 5)
-        total += div * 200
-        div, remainder = divmod(remainder, 3)
-        total += div * 130
-        total += remainder * 50
-    return total
+# #####
+# def calculate_A(total: int, item_count: dict):
+#     if "A" in item_count.keys():
+#         div, remainder = divmod(item_count["A"], 5)
+#         total += div * 200
+#         div, remainder = divmod(remainder, 3)
+#         total += div * 130
+#         total += remainder * 50
+#     return total
 
 
-def calculate_B(total: int, item_count: dict):
-    if "B" in item_count.keys():
-        div, remainder = divmod(item_count["B"], 2)
-        total += div * 45
-        total += remainder * 30
-    return total
+# def calculate_B(total: int, item_count: dict):
+#     if "B" in item_count.keys():
+#         div, remainder = divmod(item_count["B"], 2)
+#         total += div * 45
+#         total += remainder * 30
+#     return total
 
 
-def calculate_C(total: int, item_count: dict):
-    if "C" in item_count.keys():
-        total += item_count["C"] * 20
-    return total
+# def calculate_C(total: int, item_count: dict):
+#     if "C" in item_count.keys():
+#         total += item_count["C"] * 20
+#     return total
 
 
-def calculate_D(total: int, item_count: dict):
-    if "D" in item_count.keys():
-        total += item_count["D"] * 15
-    return total
+# def calculate_D(total: int, item_count: dict):
+#     if "D" in item_count.keys():
+#         total += item_count["D"] * 15
+#     return total
 
 
-def calculate_E(total: int, item_count: dict):
-    if "E" in item_count.keys():
-        div, remainder = divmod(item_count["E"], 2)
-        if "B" in item_count.keys():
-            item_count["B"] -= div
-            if item_count["B"] < 0:
-                item_count["B"] = 0
-        total += item_count["E"] * 40
-    return total
+# def calculate_E(total: int, item_count: dict):
+#     if "E" in item_count.keys():
+#         div, remainder = divmod(item_count["E"], 2)
+#         if "B" in item_count.keys():
+#             item_count["B"] -= div
+#             if item_count["B"] < 0:
+#                 item_count["B"] = 0
+#         total += item_count["E"] * 40
+#     return total
 
 
-def calculate_F(total: int, item_count: dict):
-    if "F" in item_count.keys():
-        total += item_count["F"] * 10
-        div, _ = divmod(item_count["F"], 3)
-        total -= div * 10
-    return total
+# def calculate_F(total: int, item_count: dict):
+#     if "F" in item_count.keys():
+#         total += item_count["F"] * 10
+#         div, _ = divmod(item_count["F"], 3)
+#         total -= div * 10
+#     return total
 
 
-def calculate_price(item_count: dict) -> int:
-    total = 0
-    total = calculate_A(total, item_count)
-    total = calculate_E(total, item_count)
-    total = calculate_B(total, item_count)
-    total = calculate_C(total, item_count)
-    total = calculate_D(total, item_count)
-    total = calculate_F(total, item_count)
-    return total
+# def calculate_price(item_count: dict) -> int:
+#     total = 0
+#     total = calculate_A(total, item_count)
+#     total = calculate_E(total, item_count)
+#     total = calculate_B(total, item_count)
+#     total = calculate_C(total, item_count)
+#     total = calculate_D(total, item_count)
+#     total = calculate_F(total, item_count)
+#     return total
 
 
-def checkout(skus):
-    item_count = {}
-    for item in skus:
-        if item not in ["A", "B", "C", "D", "E", "F"]:
-            return -1
-        if item in item_count.keys():
-            item_count[item] += 1
-        else:
-            item_count[item] = 1
-    return calculate_price(item_count)
+# def checkout(skus):
+#     item_count = {}
+#     for item in skus:
+#         if item not in ["A", "B", "C", "D", "E", "F"]:
+#             return -1
+#         if item in item_count.keys():
+#             item_count[item] += 1
+#         else:
+#             item_count[item] = 1
+#     return calculate_price(item_count)
+
 
 
 
