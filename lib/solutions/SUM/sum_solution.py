027bdc45161f92a868f6a601616a37734771413a
sbx_user1051 <sbx_user1051@169.254.115.113>
2023-06-17 09:57:42 +0000

    Sat Jun 17 09:57:42 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,13 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def compute(x: float, y: float) -> float:
+    """Sum two numbers.
+
+    Args:
+        x (float): Number
+        y (float): Number
+
+    Returns:
+        float: Sum result
+    """
+    return x + y
+
